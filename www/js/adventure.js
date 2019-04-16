8ab033fce75130acfcfce1a94c3c4be7fe57c8f5
John Sanders <jbsanders92@gmail.com>
2019-04-16 15:11:08 -0600

    Adventure page created
    Adventure page that links back to CMN and combat page along with a form that will be for selecting combatants

---
+++ www/js/adventure.js
@@ -0,0 +1,8 @@
+function openForm(){
+      document.getElementById("addCombatant").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("addCombatant").style.display = "none";
+  }
+
