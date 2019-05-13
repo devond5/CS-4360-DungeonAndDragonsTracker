04a8cc408f7692126644cbd158e55ca9ab74f9cd
Max Morrison <maxdmorrison6@gmail.com>
2019-05-13 17:37:01 -0600

    First pass at the combat test, needs more work on the engage page

---
+++ selenium/testCombat.py
@@ -0,0 +1,105 @@
+import unittest
+import time
+from selenium import webdriver
+
+class Testing(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Chrome()
+
+	def testCombat(self):
+		driver = self.browser
+		driver.get('http://localhost:8000/splashHome.html')
+		time.sleep(9)
+		driver.find_element_by_id('button1').click()
+		time.sleep(1)
+		driver.find_element_by_id('charOpen').click()
+		time.sleep(1)
+		driver.find_element_by_id('formButtonChar').click()
+		time.sleep(1)
+		driver.find_element_by_id('characterName').send_keys('Haladar')
+		driver.find_element_by_id('HP').send_keys('65')
+		driver.find_element_by_id('AC').send_keys('15')
+		driver.find_element_by_id('PP').send_keys('15')
+		driver.find_element_by_id('Strength').send_keys('8')
+		driver.find_element_by_id('Dexterity').send_keys('2')
+		driver.find_element_by_id('Constitution').send_keys('6')
+		driver.find_element_by_id('Intelligence').send_keys('1')
+		driver.find_element_by_id('Wisdom').send_keys('1')
+		driver.find_element_by_id('Charisma').send_keys('0')
+		driver.find_element_by_id('PlayerName').send_keys('Max')
+		driver.find_element_by_id('Background').send_keys('Made of cheese')
+		driver.find_element_by_id('ClassAndLevel').send_keys('Cleric 10')
+		driver.find_element_by_id('Experience').send_keys('55')
+		driver.find_element_by_id('Alignment').send_keys('Chaotic Neutral')
+		driver.find_element_by_id('Race').send_keys('Martian')
+		time.sleep(1)
+		driver.find_element_by_id('doneChar').click()
+		time.sleep(1)
+
+		driver.find_element_by_id('monOpen').click()
+		time.sleep(1)
+		driver.find_element_by_id('formButtonMon').click()
+		time.sleep(1)
+		driver.find_element_by_id('monsterName').send_keys('Goblin1')
+		driver.find_element_by_id('monsterHP').send_keys('35')
+		driver.find_element_by_id('monsterAC').send_keys('10')
+		driver.find_element_by_id('monsterPP').send_keys('8')
+		driver.find_element_by_id('monsterStrength').send_keys('6')
+		driver.find_element_by_id('monsterDexterity').send_keys('6')
+		driver.find_element_by_id('monsterConstitution').send_keys('0')
+		driver.find_element_by_id('monsterIntelligence').send_keys('0')
+		driver.find_element_by_id('monsterWisdom').send_keys('0')
+		driver.find_element_by_id('monsterCharisma').send_keys('1')
+		driver.find_element_by_id('monsterSavingStrength').send_keys('9')
+		driver.find_element_by_id('monsterSavingDexterity').send_keys('10')
+		driver.find_element_by_id('monsterSavingConstitution').send_keys('0')
+		driver.find_element_by_id('monsterSavingIntelligence').send_keys('0')
+		driver.find_element_by_id('monsterSavingWisdom').send_keys('0')
+		driver.find_element_by_id('monsterSavingCharisma').send_keys('10')
+		time.sleep(1)
+		driver.find_element_by_id('doneMon').click()
+		time.sleep(1)
+
+		driver.find_element_by_id('formButtonMon').click()
+		time.sleep(1)
+		driver.find_element_by_id('monsterName').send_keys('Goblin2')
+		driver.find_element_by_id('monsterHP').send_keys('35')
+		driver.find_element_by_id('monsterAC').send_keys('10')
+		driver.find_element_by_id('monsterPP').send_keys('8')
+		driver.find_element_by_id('monsterStrength').send_keys('6')
+		driver.find_element_by_id('monsterDexterity').send_keys('6')
+		driver.find_element_by_id('monsterConstitution').send_keys('0')
+		driver.find_element_by_id('monsterIntelligence').send_keys('0')
+		driver.find_element_by_id('monsterWisdom').send_keys('0')
+		driver.find_element_by_id('monsterCharisma').send_keys('1')
+		driver.find_element_by_id('monsterSavingStrength').send_keys('9')
+		driver.find_element_by_id('monsterSavingDexterity').send_keys('10')
+		driver.find_element_by_id('monsterSavingConstitution').send_keys('0')
+		driver.find_element_by_id('monsterSavingIntelligence').send_keys('0')
+		driver.find_element_by_id('monsterSavingWisdom').send_keys('0')
+		driver.find_element_by_id('monsterSavingCharisma').send_keys('10')
+		time.sleep(1)
+		driver.find_element_by_id('doneMon').click()
+		time.sleep(1)
+
+		driver.find_element_by_id('start').click()
+		time.sleep(1)
+
+		# Select the combatants
+		driver.find_element_by_id('monSelection').click()
+		time.sleep(1)
+		driver.find_element_by_id('1 monsters').click()
+		driver.find_element_by_id('2 monsters').click()
+		time.sleep(1)
+		driver.find_element_by_id('charSelection').click()
+		time.sleep(1)
+		driver.find_element_by_id('1 characters').click()
+		time.sleep(1)
+
+
+	def tearDown(self):
+		self.browser.close()
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
